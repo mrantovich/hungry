13490fe26e8239876eafb0048d18d4d23955243d
Mikhail Rantovich <mrantovich@gmail.com>
2021-04-30 11:23:05 +0700

    Полноэкранная главная секция + адаптив

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-let burgerElement = document.querySelector('.burger');
+const burgerElement = document.querySelector('.burger');
+const headerElement = document.querySelector('.header__box');
+const headerRightElement = document.querySelector('.header__box-right');
+const headerLeftElement = document.querySelector('.header__box-left');
 
 burgerElement.addEventListener('click', addActive);
 
 function addActive() {
 	this.classList.toggle('_active');
+	headerElement.classList.toggle('_active');
+	headerRightElement.classList.toggle('_active');
+	headerLeftElement.classList.toggle('_active');
 }
 
